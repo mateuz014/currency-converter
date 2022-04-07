a0025eb2557d284f6c326af3a4552045358e9b70
Mateus <mateus.a.freitas2830@gmail.com>
2022-04-07 18:22:21 -0300

    n sei o q to fazendo kkk

--- js/index.js
+++ js/index.js
@@ -3,7 +3,21 @@ const currencyTwoEl = document.querySelector('[data-js="currency-two"]')
 
 const url = 'https://v6.exchangerate-api.com/v6/aacbfd8276f71f627217b4ec/latest/USD'
 
-fetch(url)
+const fetchExchangeRate = async () =>{
+    try {
+    const response = await fetch(url)
+    const exchangeRateData = await response.json()
+
+    if (exchangeRateData.result === 'error'){
+        throw new Error('Could not get the information')
+    }
+        
+    }catch(err){
+        alert(err.message)
+    }
+}
+
+fetchExchangeRate()
 
 const option = `<option>oi</option>`
 
